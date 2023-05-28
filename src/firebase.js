ac31b61f886bb2d6adf341a67b9d53f2e8cbd2d4
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-05-28 21:18:35 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCw9ofkdtO8trpxY4vFNl3yVKrv0OwFe-E",
+    authDomain: "whatsappclone-ff0eb.firebaseapp.com",
+    projectId: "whatsappclone-ff0eb",
+    storageBucket: "whatsappclone-ff0eb.appspot.com",
+    messagingSenderId: "869363550836",
+    appId: "1:869363550836:web:35bbf536a17184c0ff1f21"
+};
